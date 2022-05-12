c97aac3ea707115a03db1737d99f4b887bb98d60
AlexiaBer <96543341+AlexiaBer@users.noreply.github.com>
2022-05-12 10:04:38 +0200

    Add files via upload
    first commit

---
+++ front/js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+fetch(“http://localhost:3000/api/products")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+  })
+  .catch(function(err) {
+  });
+
+});
+
+
+
+
